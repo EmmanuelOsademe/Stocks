6974269797d3815c1b7f360b8b7b55e79ae764bd
Emmanuel Osademe <emma.osademe@gmail.com>
2024-04-02 00:27:57 +0100

    more work

---
+++ Mappers/StockMapper.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock stockModel){
+            return  new StockDto {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDto (this CreateStockRequestDto requestStock){
+            
+            return new Stock {
+                Symbol = requestStock.Symbol,
+                CompanyName = requestStock.CompanyName,
+                Purchase = requestStock.Purchase,
+                LastDiv = requestStock.LastDiv,
+                Industry = requestStock.Industry,
+                MarketCap = requestStock.MarketCap
+            };
+        }
+    }
+}
