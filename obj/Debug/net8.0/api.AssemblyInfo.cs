9bada392a76f43933a6ca7747227084e324955f7
Emmanuel Osademe <emma.osademe@gmail.com>
2024-04-05 09:54:06 +0100

    updated connection string

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b502e94084b0c85c46c770421ea7e1722e561588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30368f41e3ca776b65d027e9af3603899d603eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
